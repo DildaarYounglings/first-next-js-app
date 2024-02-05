73f1b8bb482e6b41e03198cd14cbe513424680f3
primedildaar2 <primedildaar2@gmail.com>
2024-02-05 15:23:16 +0200

    creating the folder structure

--- app/api/users/route.js
+++
@@ -1,4 +0,0 @@
-export async function GET(request){
-    const v = [{name:"Bruce Wayne",password:"123"},{name:"Son Goku",password:"123"}, {name:"Clark Kent",password:"123"},{name:"Dextor Abrahams",password:"123"}];
-    return new Response(JSON.stringify(v));
-}
